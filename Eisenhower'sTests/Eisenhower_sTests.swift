1b0bcb4d231a25e793298e6af79129a972da7d4b
batu <114383125+Batu-end@users.noreply.github.com>
2026-05-13 16:15:06 -0700

    chore: integrate Xcode project and organize source files

---
+++ Eisenhower'sTests/Eisenhower_sTests.swift
@@ -0,0 +1,19 @@
+//
+//  Eisenhower_sTests.swift
+//  Eisenhower'sTests
+//
+//  Created by Batu Demirtas on 5/13/26.
+//
+
+import Testing
+@testable import Eisenhower_s
+
+struct Eisenhower_sTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
